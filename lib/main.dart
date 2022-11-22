e8e5922d49f61716fd5f0bea2507fb476556ad3d
Miguel Ramos <return.infox@gmail.com>
2022-11-22 20:01:47 -0300

    creando acceso al provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rickandmortyapi/pages/pages.dart';
+import 'package:rickandmortyapi/provider/characters_provider.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(const AppState());
+}
+
+//* Creamos un nuevo widget que va ser el que maneje el estado del provider
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+      ChangeNotifierProvider(create: (context) => CharactersProvider(), lazy: false)
+    ],
+    child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
